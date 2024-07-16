aedccb857e49c90993a450ad8b57da4410a25fda
Syed Khalid Hussain <124891368+syedkhalid0@users.noreply.github.com>
2024-07-16 09:08:54 +0530

    Update BlogHeader.tsx
    Updated Blog Header "home: link"

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -13,7 +13,7 @@ export default function App() {
     <div class="w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
+          <a href="https://sandbox.syedkhalid.me" class="block py-2 px-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
         </li>
       </ul>
     </div>
